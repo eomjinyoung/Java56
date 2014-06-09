832c4d08ff89f545ec5c0e30ed1417c0e1839003
Administrator <Administrator@host-PC>
2014-06-09 13:52:12 +0900

    .

---
+++ java01t/src/exam/oop/instance/Calculator01.java
@@ -0,0 +1,23 @@
+//1. 클래스 용도 : 메서드들의 묶음.
+
+package exam.oop.instance;
+
+public class Calculator01 {
+
+	public static int plus(int a, int b) {
+		return a + b;
+	}
+
+	public static int minus(int a, int b) {
+		return a - b;
+	}
+	
+	public static int multiple(int a, int b) {
+		return a * b;
+	}
+	
+	
+	public static int divide(int a, int b) {
+		return a / b;
+	}
+}
