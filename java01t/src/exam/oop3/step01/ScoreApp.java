14588743911307646d227a36674460149a5af3e6
Jinyoung Eom <jinyoung.eom@gmail.com>
2014-06-13 16:15:18 +0900

    .

--- java01t/src/exam/oop3/step01/ScoreApp.java
+++ java01t/src/exam/oop3/step01/ScoreApp.java
@@ -1,27 +1,15 @@
 package exam.oop3.step01;
 
-/* 윈도우 이벤트 처리를 위한 WindowListener 사용
- * - 윈도우 이벤트가 발생했을 때 호출되는 메서드 규칙을 정한 것이다.
- * - 자바의 인터페이스 문법으로 정의했다.
- * - 인터페이스 문법
- *   interface 규칙이름 {
- *     메서드 시그너처;
- *     ...
- *   }
+/* ScoreFrame 사용
+ * - 상속 기법을 이용하여 Frame 기능을 확장함.
  * 
  */
-
-import java.awt.Frame;
 
 public class ScoreApp {
 
 	public static void main(String[] args) {
-		Frame f = new Frame();
-		f.setTitle("비트 성적관리 시스템");
-		f.setSize(400, 300);
-		f.addWindowListener(new MyWindowListener());
+		ScoreFrame f = new ScoreFrame();
 		f.setVisible(true);
-
 	}
 
 }
