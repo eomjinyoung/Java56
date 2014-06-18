073ba4f2c9ad8857072232aded1c7c06e20d0943
jinyoung <jinyoung.eom@gmail.com>
2014-06-18 14:24:26 +0900

    .

---
+++ java01t/src/exam/oop3/step05/ScoreDao.java
@@ -0,0 +1,59 @@
+/* 배열 대신 ArrayList 적용 
+ */
+package exam.oop3.step05;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+public class ScoreDao {
+  ArrayList<Score> scores = new ArrayList<Score>();
+  int cursor;
+
+  public ScoreDao() {
+  }
+
+  public void insert(Score score) {
+    scores.add(score);
+    cursor = scores.size();
+  }
+
+  public Score nextScore() {
+    if (cursor >= scores.size() - 1) 
+      return null;
+
+    return scores.get(++cursor);
+  }
+
+  public Score previousScore() {
+    if (cursor <= 0) 
+      return null;
+
+    return scores.get(--cursor);
+  }
+
+  public Object[] toArray() {
+    return this.scores.toArray();
+  }
+
+  public void save() {
+    File data = new File("test.csv");
+    FileWriter out = new FileWriter(data);
+    
+    for (Score score : scores) {
+      
+    }
+    
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
