ea182d173a88a161ee551bf1c4a9df7204bd42b7
jinyoung <jinyoung.eom@gmail.com>
2014-06-18 13:34:26 +0900

    .

--- java01t/src/exam/oop3/step04/ScoreDao.java
+++ java01t/src/exam/oop3/step04/ScoreDao.java
@@ -5,39 +5,35 @@ package exam.oop3.step04;
 import java.util.ArrayList;
 
 public class ScoreDao {
-	ArrayList<Score> scores = new ArrayList<Score>();
-	int size;
-	int cursor;
-	
-	public ScoreDao() {
-		//size = 0;
-	}
-	
-	public void insert(Score score) {
-		if (size < scores.length) {
-			scores[size++] = score;
-			cursor = size;
-		}
-	}
-	
-	public Score nextScore() {
-		if (cursor >= size || cursor >= scores.length - 1) 
-			return null;
-		
-		return scores[++cursor];
-	}
-	
-	public Score previousScore() {
-		if (cursor <= 0) 
-			return null;
-		
-		return scores[--cursor];
-	}
-	
-	public Score[] toArray() {
-		return this.scores;
-	}
-	
+  ArrayList<Score> scores = new ArrayList<Score>();
+  int cursor;
+
+  public ScoreDao() {
+  }
+
+  public void insert(Score score) {
+    scores.add(score);
+    cursor = scores.size();
+  }
+
+  public Score nextScore() {
+    if (cursor >= scores.size() - 1) 
+      return null;
+
+    return scores.get(++cursor);
+  }
+
+  public Score previousScore() {
+    if (cursor <= 0) 
+      return null;
+
+    return scores.get(--cursor);
+  }
+
+  public Object[] toArray() {
+    return this.scores.toArray();
+  }
+
 }
 
 
