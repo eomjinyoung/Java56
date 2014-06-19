36d44b6bc7481449cd62ddf42b0b71ada59a8d89
jinyoung <jinyoung.eom@gmail.com>
2014-06-19 10:33:44 +0900

    .

--- java01t/src/exam/oop3/step05/File02.java
+++ java01t/src/exam/oop3/step05/File02.java
@@ -36,20 +36,17 @@ public class File02 {
     
     
     System.out.println("(실무)디렉토리만 출력-------------------------");
-    class DirectoryFilter implements FilenameFilter {
-      public boolean accept(File dir, String name) {
-        //File f = new File(dir.getAbsolutePath() + "\\" + name);
-        /*File f = new File(dir, name);
-        if (f.isDirectory()) {
-          
-        }*/
-        if ( new File(dir, name).isDirectory() ) return true;
-        else return false;
-      }
-    }
-    for (String name : f.list( )) {
+    for (String name : f.list( new FilenameFilter() {
+        public boolean accept(File dir, String name) {
+          if ( new File(dir, name).isDirectory() ) return true;
+          else return false;
+        }
+      } )) {
       System.out.println(name);
     }
+    
+    System.out.println("listFiles() 사용 ------------------------");
+    
 
   }
 
