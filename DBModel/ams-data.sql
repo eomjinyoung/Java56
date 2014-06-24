d5df03b65bbc2c5265f3703aad7731fc15fc6725
jinyoung <jinyoung.eom@gmail.com>
2014-06-24 10:18:04 +0900

    .

---
+++ DBModel/ams-data.sql
@@ -0,0 +1,19 @@
+insert into  MEMBERS (NAME, EMAIL, TEL, PWD)
+values ('aaaa', 'aaaa@test.com', '1111-1111', '1111'); 
+
+insert into  MEMBERS (NAME, EMAIL, TEL, PWD)
+values ('홍길동', 's1@test.com', '1111-1111', '1111'); 
+
+insert into  MEMBERS (NAME, EMAIL, TEL, PWD)
+values ('임꺽정', 's2@test.com', '1111-2222', '1111'); 
+
+insert into  MEMBERS (NAME, EMAIL, TEL, PWD)
+values ('장보고', 's3@test.com', '1111-3333', '1111'); 
+
+insert into  MEMBERS (NAME, EMAIL, TEL, PWD)
+values ('이순신', 's4@test.com', '1111-4444', '1111'); 
+
+insert into  MEMBERS (NAME, EMAIL, TEL, PWD)
+values ('유관순', 's5@test.com', '1111-5555', '1111'); 
+
+
