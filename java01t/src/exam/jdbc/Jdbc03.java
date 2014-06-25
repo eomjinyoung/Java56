0a1bc489e80d19ac2fd6e348dafea2721e57429d
jinyoung <jinyoung.eom@gmail.com>
2014-06-25 15:37:46 +0900

    .

--- java01t/src/exam/jdbc/Jdbc03.java
+++ java01t/src/exam/jdbc/Jdbc03.java
@@ -5,26 +5,24 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-/* INSERT 처리 
+/* UPDATE 처리 
  */
 
-public class Jdbc02 {
+public class Jdbc03 {
 
   public static void main(String[] args) throws Exception {
     Class.forName("com.mysql.jdbc.Driver");
     Connection con = DriverManager.getConnection(
-        "jdbc:mysql://localhost:3306/bitdb?useUnicode=true&characterEncoding=UTF-8", 
-        "bit", "1111"   );
+        "jdbc:mysql://localhost:3306/bitdb", "bit", "1111"   );
     Statement stmt = con.createStatement();
     
     // executeUpdate() : INSERT, UPDATE, DELETE 문 수행 
     // - 리턴 값은 생성되거나, 변경되거나, 삭제된 레코드의 수
     int count = stmt.executeUpdate(
-        "insert into members (name, email, tel, pwd)"
-        + " values ('윤봉길', 's11@test.com', '111-1010', '1111')");
+        "update members set tel='010-1111-1234' where mno=10");
     
     if (count == 1) {
-      System.out.println("입력 완료!");
+      System.out.println("변경 완료!");
     }
     
     stmt.close();
