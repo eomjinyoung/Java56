7d1e84dec9639849424375a7b4ac88fa1a61a8c4
jinyoung <jinyoung.eom@gmail.com>
2014-07-11 11:02:05 +0900

    .

--- web01t/src/servlets/step07/ContextLoaderListener.java
+++ web01t/src/servlets/step07/ContextLoaderListener.java
@@ -2,12 +2,9 @@ package servlets.step07;
 
 import java.io.InputStream;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.sql.DataSource;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -24,12 +21,6 @@ public class ContextLoaderListener implements ServletContextListener {
     try {
       ServletContext ctx = event.getServletContext();
       
-      // 서버의 자원을 찾아주는 도구 준비
-      Context initCtx = new InitialContext();
-      // lookup(): 서버 자원을 찾아서 리턴함.
-      DataSource dataSource = (DataSource)initCtx.lookup(
-          "java:/comp/env/jdbc/bitdb");
-      
       // MyBatis 데이터 처리 대행 객체 준비
       // 1) 설정 파일을 읽어들일 때 사용할 도구(InputStream)를 준비한다.
       // 2) SqlSessionBuilder를 준비한다.
@@ -43,7 +34,6 @@ public class ContextLoaderListener implements ServletContextListener {
           new SqlSessionFactoryBuilder().build(inputStream);
       
       ScoreDao scoreDao = new ScoreDao();
-      scoreDao.setDataSource(dataSource);
       scoreDao.setSqlSessionFactory(sqlSessionFactory);
       
       // 페이지 컨트롤러를 ServletContext에 보관함.
--- web01t/src/servlets/step07/ScoreDao.java
+++ web01t/src/servlets/step07/ScoreDao.java
@@ -2,25 +2,13 @@
  */
 package servlets.step07;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
-
-import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 public class ScoreDao {
-  DataSource dataSource;
   SqlSessionFactory sqlSessionFactory;
-  
-  public void setDataSource(DataSource dataSource) {
-    this.dataSource = dataSource;
-  }
   
   public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
     this.sqlSessionFactory = sqlSessionFactory;
@@ -53,89 +41,51 @@ public class ScoreDao {
   }
 
   public void insert(Score score) throws Exception {
-    Connection con = null;
-    PreparedStatement stmt = null;
-    ResultSet rs = null; // 자동 생성된 PK 값을 가져오는 역할자
+    SqlSession sqlSession = sqlSessionFactory.openSession();
     
     try {
-      con = dataSource.getConnection();
-
-      stmt = con.prepareStatement(
-          "insert into scores (name, kor, eng, math)" +
-          " values(?, ?, ?, ?)", 
-          Statement.RETURN_GENERATED_KEYS);
-
-      // IN-PARAMETER의 타입에 따라 setXxxx(인덱스, 값) 호출한다.
-      stmt.setString(1, score.getName()); 
-      stmt.setInt(2, score.getKor());
-      stmt.setInt(3, score.getEng());
-      stmt.setInt(4, score.getMath());
-      
-      int count = stmt.executeUpdate();
-      
-      if (count == 1) {
-        // 입력 성공 후에 자동 생성된 PK 값 가져오기
-        rs = stmt.getGeneratedKeys(); //1) 자동 생성 PK값 가져오는 역할자 얻기 
-        rs.next(); // 2) PK 값 가져오기 
-        score.setNo( rs.getInt(1)); // 3) DBMS에서 자동 생성된 PK 값을 Score에 저장. 
-      }
+      sqlSession.insert("servlets.step07.ScoreDao.insert", score);
+      sqlSession.commit();
       
     } catch (Exception e) {
+      sqlSession.rollback();
       throw e;
       
-    } finally { 
-      try { rs.close();} catch (SQLException e) {}
-      try { stmt.close();} catch (SQLException e) {}
-      try { con.close();} catch (SQLException e) {}
+    } finally {
+      sqlSession.close();
     }
   }
 
   public int delete(int no) throws Exception {
-    Connection con = null;
-    PreparedStatement stmt = null;
+    SqlSession sqlSession = sqlSessionFactory.openSession();
     
     try {
-      con = dataSource.getConnection();
-      stmt = con.prepareStatement( 
-          "delete from scores where sno = ?");
-      
-      stmt.setInt(1, no);
-      
-      return stmt.executeUpdate();
-      
+      int count = sqlSession.delete("servlets.step07.ScoreDao.delete", no);
+      sqlSession.commit();
+      return count;
       
     } catch (Exception e) {
+      sqlSession.rollback();
       throw e;
       
     } finally { 
-      try { stmt.close();} catch (SQLException e) {}
-      try { con.close();} catch (SQLException e) {}
+      sqlSession.close();
     }
   }
 
   public void update(Score score) throws Exception {
-    Connection con = null;
-    PreparedStatement stmt = null;
+    SqlSession sqlSession = sqlSessionFactory.openSession();
     
     try {
-      con = dataSource.getConnection();
-      stmt = con.prepareStatement(
-          "update scores set name=?, kor=?, eng=?, math=? where sno=?");
-      
-      stmt.setString(1, score.getName());
-      stmt.setInt(2, score.getKor());
-      stmt.setInt(3, score.getEng());
-      stmt.setInt(4, score.getMath());
-      stmt.setInt(5, score.getNo());
-      
-      stmt.executeUpdate();
+      sqlSession.update("servlets.step07.ScoreDao.update", score);
+      sqlSession.commit();
       
     } catch (Exception e) {
+      sqlSession.rollback();
       throw e;
       
     } finally { 
-      try { stmt.close();} catch (SQLException e) {}
-      try { con.close();} catch (SQLException e) {}
+      sqlSession.close();
     }
   }
 
