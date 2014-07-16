525ff573be932266af8d1115c27aa7681f1271bb
jinyoung <jinyoung.eom@gmail.com>
2014-07-16 11:09:04 +0900

    .

---
+++ Spring01/src/main/java/step01/Car.java
@@ -0,0 +1,32 @@
+package step01;
+
+public class Car {
+  String  maker;
+  String  model;
+  int     capacity;
+  
+  @Override
+  public String toString() {
+    return "Car [maker=" + maker + ", model=" + model + ", capacity="
+        + capacity + "]";
+  }
+  
+  public String getMaker() {
+    return maker;
+  }
+  public void setMaker(String maker) {
+    this.maker = maker;
+  }
+  public String getModel() {
+    return model;
+  }
+  public void setModel(String model) {
+    this.model = model;
+  }
+  public int getCapacity() {
+    return capacity;
+  }
+  public void setCapacity(int capacity) {
+    this.capacity = capacity;
+  }
+}
