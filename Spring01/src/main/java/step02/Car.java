3d54716effaa5ba997c2c87d9f245606e132fe93
jinyoung <jinyoung.eom@gmail.com>
2014-07-16 14:08:58 +0900

    .

--- Spring01/src/main/java/step02/Car.java
+++ Spring01/src/main/java/step02/Car.java
@@ -5,15 +5,19 @@ public class Car {
   String  model;
   int     capacity;
   
-  public Car() { }
+  public Car() { 
+    System.out.println("Car() 호출");
+  }
   
   public Car(String maker, String model) {
+    System.out.println("Car(maker,model) 호출");
     this.maker = maker;
     this.model = model;
   }
   
   public Car(String maker, String model, int capacity) {
     this(maker, model);
+    System.out.println("Car(maker,model,capacity) 호출");
     this.capacity = capacity;
   }
   
@@ -27,18 +31,21 @@ public class Car {
     return maker;
   }
   public void setMaker(String maker) {
+    System.out.println("setMaker() 호출");
     this.maker = maker;
   }
   public String getModel() {
     return model;
   }
   public void setModel(String model) {
+    System.out.println("setModel() 호출");
     this.model = model;
   }
   public int getCapacity() {
     return capacity;
   }
   public void setCapacity(int capacity) {
+    System.out.println("setCapacity() 호출");
     this.capacity = capacity;
   }
 }
---
+++ Spring01/src/main/java/step02/Test03.java
@@ -0,0 +1,22 @@
+package step02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test03 {
+  public static void main(String[] args) {
+    ApplicationContext ctx = new ClassPathXmlApplicationContext(
+            new String[]{"step02/application-context3.xml"});
+    
+    Car c1 = (Car)ctx.getBean("c1"); 
+    Car c2 = (Car)ctx.getBean("c2"); 
+    
+    System.out.println(c1);
+    System.out.println(c2);
+  }
+}
+
+
+
+
+
