2639b48165ce3603a8c28a8c103a9db102153cda
jinyoung <jinyoung.eom@gmail.com>
2014-07-16 16:21:38 +0900

    .

---
+++ Spring01/src/main/java/step04/Test03.java
@@ -0,0 +1,21 @@
+package step04;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test03 {
+  public static void main(String[] args) {
+    ApplicationContext ctx = new ClassPathXmlApplicationContext(
+            new String[]{"step04/application-context3.xml"});
+    
+    Car3 c1 = (Car3)ctx.getBean("c1"); 
+    System.out.println(c1);
+    
+  }
+}
+
+
+
+
+
+
