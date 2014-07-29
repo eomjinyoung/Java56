38b1c1b9010c378e2513954a5280875c08ac5536
jinyoung <jinyoung.eom@gmail.com>
2014-07-29 15:00:20 +0900

    .

--- web02t/src/main/webapp/WEB-INF/view/score/json/ScoreList.jsp
+++ web02t/src/main/webapp/WEB-INF/view/score/json/ScoreList.jsp
@@ -1,10 +1,10 @@
-<%@page import="com.google.gson.Gson"%>
-<%@page import="java56.vo.Score"%>
-<%@page import="java.util.List"%>
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %> 
-<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<%@ page import="com.google.gson.Gson"%>
+<%@ page import="java56.vo.Score"%>
+<%@ page import="java.util.List"%>
+<%@ page language="java" 
+    contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"
+    trimDirectiveWhitespaces="true"%>
 <%
 List<Score> scores = (List<Score>)request.getAttribute("scores");
 %>
--- web02t/src/main/webapp/score/app.js
+++ web02t/src/main/webapp/score/app.js
@@ -1,3 +1,70 @@
-/**
- * 
- */
+window.onload = function() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(event) {
+	if (xhr.readyState == 4) {
+	  //1. 서버로부터 받은 JSON 문자열을 실행하여 자바스크립트 객체로 만든다.
+	  var scores = eval('(' + xhr.responseText + ')');
+	  
+	  //2. 배열을 반복하면서 성적정보 출력
+	  var table = document.getElementById("scoreTable");
+	  var tr, td, a;
+	  for (var i in scores) {
+		scores[i].total = scores[i].kor + scores[i].eng + scores[i].math;
+		scores[i].average = scores[i].total / 3;
+		
+		tr = document.createElement("tr");
+		table.appendChild(tr);
+		
+		td = document.createElement("td");
+		a = document.createElement("a");
+		a.href = 'update.do?no=' + scores[i].no;
+		a.textContent = scores[i].no;
+		td.appendChild(a);
+		tr.appendChild(td);
+		
+		td = document.createElement("td");
+		td.textContent = scores[i].execDate;
+		tr.appendChild(td);
+		
+		td = document.createElement("td");
+		td.textContent = scores[i].name;
+		tr.appendChild(td);
+		
+		td = document.createElement("td");
+		td.textContent = scores[i].kor;
+		tr.appendChild(td);
+		
+		td = document.createElement("td");
+		td.textContent = scores[i].eng;
+		tr.appendChild(td);
+		
+		td = document.createElement("td");
+		td.textContent = scores[i].math;
+		tr.appendChild(td);
+		
+		td = document.createElement("td");
+		td.textContent = scores[i].total ;
+		tr.appendChild(td);
+		
+		td = document.createElement("td");
+		td.textContent = scores[i].average;
+		tr.appendChild(td);
+		
+		td = document.createElement("td");
+		a = document.createElement("a");
+		a.href = 'delete.do?no=' + scores[i].no;
+		a.textContent = '삭제';
+		td.appendChild(a);
+		tr.appendChild(td);
+	  }
+	}
+  };
+  
+  xhr.open('GET', 'list.json', true);
+  xhr.send(null);
+};
+
+
+
+
+
